ce5c592e371cc2edbf94e4c36ae118b9d7982c07
thovinasa <48348697+thovinasa@users.noreply.github.com>
2019-04-04 10:30:00 +0530

    Add files via upload

--- wrdtoimage.py
+++ wrdtoimage.py
@@ -2,14 +2,14 @@ from PIL import ImageFont, ImageDraw, Image
 import at
 def txt_to_img(txt,out):
 	image = Image.new('RGB', (110, 110), color = (0, 0, 0))
-	fontsize = 1 
+	fontsize = 40 
 
 	img_fraction = 0.5
 
 	font = ImageFont.truetype('open-sans/OpenSans-Regular.ttf', fontsize)
-	while font.getsize(txt)[0] < img_fraction*image.size[0]:
-	    fontsize += 1
-	    font = ImageFont.truetype('open-sans/OpenSans-Regular.ttf', fontsize)
+	#while font.getsize(txt)[0] < img_fraction*image.size[0]:
+	#    fontsize += 1
+	#    font = ImageFont.truetype('open-sans/OpenSans-Regular.ttf', fontsize)
 
 	fontsize -= 1
 	draw = ImageDraw.Draw(image)
